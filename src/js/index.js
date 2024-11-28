f7304eb8681dbd83683d50c7632f6f78cbbb5a1e
Olga Samylina <o.s.melnik@mail.ru>
2024-11-28 23:06:43 +0300

    one commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
 import '../style/style.scss';
+
+
+const form = document.getElementById('contact-form');
+const messageDiv = document.getElementById('message');
+
+
+form.addEventListener('sgetstarted', function(event) {
+    event.preventDefault();
+    const name = getElementById('name').value;
+
+    messageDiv.textContent = `Thanks, ${name}, our managers will contact you as soon as possible`;
+
+    form.reset();
+});
+
+
+
+/*document.getElementById('contact-form').addEventListener('getstarted', function(event) {
+    event.preventDefault();
+
+    const name = getElementById('name').value;
+    const phone = getElementById('phone').value;
+    const comment = getElementById('comment').value;
+
+    const responseMessage = `Thanks, ${name}, our managers will contact you as soon as possible`;
+
+    document.getElementById('response-message').innerText = responseMessage;
+});
+*/
