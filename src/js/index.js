60829f9e1388d82f74a528ca1af2f2ad1fe6757c
Olga Samylina <o.s.melnik@mail.ru>
2024-11-30 11:01:14 +0300

    one commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import '../style/style.scss';
 
-
+//форма обратной связи
 const form = document.getElementById('contact-form');
 const messageDiv = document.getElementById('message');
 
@@ -15,6 +15,25 @@ form.addEventListener('sgetstarted', function(event) {
 });
 
 
+
+//смена картинки
+
+
+
+const image = document.getElementById('image');
+const newImageSrc = '.img/hall2.svg'; // Путь к новому изображению
+let timeoutId;
+
+image.addEventListener('mouseenter', () => {
+    timeoutId = setTimeout(() => {
+        image.src = newImageSrc;
+    }, 2000); // Задержка 2 секунды
+});
+
+image.addEventListener('mouseleave', () => {
+    clearTimeout(timeoutId);
+    image.src = '.img/hall.svg'; // Возврат к оригинальному изображению
+});
 
 /*document.getElementById('contact-form').addEventListener('getstarted', function(event) {
     event.preventDefault();
