43aedfccc190ca6c310845e9947f529c30f785be
Olga Samylina <o.s.melnik@mail.ru>
2024-12-15 19:20:30 +0300

    one commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,18 @@
 //import '../style/style.css';
 
 
-//меняем изображение
 
-const images = [
-    {
-        normal: './img/hall.svg',
-        hover: './img/hall2.svg',
-    },
-    {
-        normal: './img/room.svg',
-        hover: './img/room2.svg',
-    },
-    {
-        normal: './img/kamin.svg',
-        hover: './img/kamin2.svg',
-    },
-    {
-        normal: './img/bedroom.svg',
-        hover: './img/bedroom2.svg',
-    },
-];
 
-const items = document.querySelectorAll('.rooms');
 
-items.forEach((item, index) => {
-    const img = item.querySelector('.rooms-img-left, .rooms-img-right');
 
-    item.addEventListener('mouseenter', () => {
-        img.src = images[index].hover;
-    });
+//меню
 
-    item.addEventListener('mouseleave', () => {
-        img.src = images[index].normal;
-    });
+
+document.getElementById('burger').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
 });
-
-
-
-// форма обратной связи
-
-const form = document.getElementById('contactForm');
-const messageDiv = document.getElementById('thankYouMessage');
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); // предотвращаем стандартное поведение формы (перезагрузку страницы)
-    messageDiv.style.display = 'block'; // показываем сообщение
-    form.reset(); // сбрасываем форму, если это необходимо
+document.getElementById('close-button').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.remove('active');
 });
