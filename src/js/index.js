9f2f96efb098faf043e0f3de416571e579690712
Olga Samylina <o.s.melnik@mail.ru>
2024-12-17 00:21:26 +0300

    hero

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,1 @@
 //import '../style/style.css';
-
-
-
-
-
-
-//меню
-
-
-document.getElementById('burger').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('active');
-});
-document.getElementById('close-button').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.remove('active');
-});
