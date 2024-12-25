2563e0ad68e5e418cc70844ad392824adc3d4347
yriidanilov <relantii@mail.ru>
2024-12-25 14:44:20 +0300

    feat/create section connection

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import { fallMenu } from './fallMenu.js'
 import { splitWords } from './animationLetters.js'
 import { preloader } from './preloader.js'
+import {feedback} from  './feedback.js';
 
 fallMenu()
 splitWords()
 preloader()
+feedback()
