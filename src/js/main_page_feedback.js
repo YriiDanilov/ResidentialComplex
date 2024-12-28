15bba52f9a26ee68184e50d05b1e065b13ba8f06
yriidanilov <relantii@mail.ru>
2024-12-28 20:30:55 +0300

    feat/create adaptiv section interior

--- src/js/main_page_feedback.js
+++ src/js/main_page_feedback.js
@@ -8,6 +8,8 @@ export const feedback = () => {
         const textSuccess = document.querySelector('#connection__feedback')
         const body = document.body
         const header = document.querySelector('.header')
+        const btnFeedBack = document.querySelector('#btn-feedback')
+        let scrollPosition = 0
 
         const closeConnection = () => {
             body.classList.remove('popup-fade')
@@ -16,10 +18,45 @@ export const feedback = () => {
             form.reset()
             header.classList.remove('z0')
             btnShow.classList.remove('z0')
+            body.style.position = ''
+            body.style.top = ''
+            window.scrollTo(0, scrollPosition)
+        }
+
+        const safePosition = () =>{
+            scrollPosition = window.scrollY
+            body.style.top = `-${scrollPosition}px`
+            body.style.position = 'fixed'
         }
 
 
         btnShow.addEventListener('click', () => {
+            safePosition()
+            btnShow.classList.toggle('z0')
+            header.classList.toggle('z0')
+            body.classList.toggle('popup-fade')
+            body.classList.toggle('no-scroll')
+            connectionWrapper.classList.add('openForm')
+
+            if (connectionWrapper.contains(textSuccess)) {
+                textSuccess.remove()
+                wrapperForm.append(form)
+            }
+            
+        })
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            form.replaceWith(textSuccess)
+        })
+
+        btnClose.addEventListener('click', () => {
+            closeConnection()
+        })
+
+        btnFeedBack.addEventListener('click', () => {
+            safePosition()
+
             btnShow.classList.toggle('z0')
             header.classList.toggle('z0')
             body.classList.toggle('popup-fade')
@@ -31,29 +68,12 @@ export const feedback = () => {
                 wrapperForm.append(form)
             }
         })
+    })
 
-        form.addEventListener('submit', (e) => {
-    
-            e.preventDefault()
-            header.classList.toggle('z0')
-            form.replaceWith(textSuccess)
-            body.classList.toggle('noscroll')
-            header.classList.toggle('z0')
-            
-            btnShow.classList.toggle('z0')
-        })
-
-        btnClose.addEventListener('click', () => {
+    document.addEventListener('click', (e) => {
+        const clickInsideMenu = connectionWrapper.contains(e.target) || btnShow.contains(e.target)
+        if (!clickInsideMenu) {
             closeConnection()
-        })
-
-        document.addEventListener('click', (e) => {
-            const clickInsideMenu = connectionWrapper.contains(e.target) || btnShow.contains(e.target)
-            if (!clickInsideMenu) {
-                closeConnection()
-            }
-        })
-
-        
+        }
     })
 }
--- src/js/main_page_index.js
+++ src/js/main_page_index.js
@@ -2,8 +2,10 @@ import { fallMenu } from './main_page_fallMenu.js'
 import { splitWords } from './main_page_animationLetters.js'
 import { preloader } from './main_page_preloader.js'
 import { feedback } from './main_page_feedback.js'
+import { swiper } from './main_page_swiperSettings.js'
 
 fallMenu()
 splitWords()
 preloader()
 feedback()
+swiper;
