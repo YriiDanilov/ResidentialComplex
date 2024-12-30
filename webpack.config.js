a1ef7efd30bd0f65562403bc1fe6dfebd244e238
yriidanilov <relantii@mail.ru>
2024-12-30 18:00:10 +0300

    set webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,66 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production'
+}
+
+console.log(mode + ' mode')
 
 module.exports = {
-    entry: './src/js/index.js',  // Точка входа (главный файл проекта)
+    mode: mode,
+    entry: './src/js/main_page_index.js',
     output: {
-        filename: 'bundle.js',  // Выходной файл для JS
-        path: path.resolve(__dirname, 'dist'),  // Папка для выходных файлов
+        assetModuleFilename: 'assets/[hash][ext][query]',
+        clean: true,
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve('./src/main_page.html'),
+        }),
+    ],
     module: {
         rules: [
             {
-                test: /\.js$/,  // Транспиляция JS
+                test: /\.html$/i,
+                loader: 'html-loader', //html загрузчик
+            },
+            {
+                test: /\.(c|sc|sa)ss$/i,
+                use: [
+                    mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader, // css загрузчик
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader', // vendored prefix
+                        options: {
+                            postcssOptions: {
+                                plugins: ['postcss-preset-env'],
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif|webp)$/i, // img
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i, //fonts
+                type: 'asset/resource',
+            },
+            // {
+            //     test: /.\pug$/i,
+            //     loader: 'pug-loader',
+            //     exclude: /(node_modules|bower_components)/,
+            // },
+            {
+                test: /\.m?js$/i,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
@@ -21,40 +69,6 @@ module.exports = {
                     },
                 },
             },
-            {
-                test: /\.scss$/,  // Преобразование Sass в CSS
-                use: [
-                    MiniCssExtractPlugin.loader,  // Используем плагин для извлечения CSS
-                    'css-loader',                  // Обрабатываем CSS
-                    'sass-loader',                 // Компилируем Sass в CSS
-                ],
-            },
-            {
-                test: /\.css$/,  // Преобразование обычного CSS
-                use: [
-                    MiniCssExtractPlugin.loader,  // Используем плагин для извлечения CSS
-                    'css-loader',                 // Обрабатываем CSS
-                ],
-            },
         ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',  // Шаблон для генерации HTML
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'styles.css',  // Название файла для сгенерированного CSS
-        }),
-    ],
-    optimization: {
-        minimize: true,  // Включаем минимизацию
-        minimizer: [
-            new CssMinimizerPlugin(),  // Плагин для минимизации CSS
-        ],
-    },
-    devServer: {
-        static: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000,
-    },
-};
+}
