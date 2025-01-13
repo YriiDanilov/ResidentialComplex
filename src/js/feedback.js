17fccf5f7a909da9c7502c51a6d1dbac075ca382
Igor <kostyuchencko.igor2015@yandex.ru>
2025-01-13 20:06:44 +0300

    feat/доделал страницу

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -4,38 +4,39 @@ export const feedback = () => {
 
     document.addEventListener('DOMContentLoaded', () => {
     
-         const btnShow = document.querySelector('#btn-show');
-         const wrapperConnection = document.querySelector('#wrapper-connection');
-         const btnClose = document.querySelector('#btn-close');
-        const form = document.querySelector('#form');
-    
-    
-         btnShow.addEventListener('click', (e) => {
-            e.preventDefault();
-            wrapperConnection.classList.toggle('openForm')
-        })
-    
-        const textSuccess = document.createElement('p')
-        textSuccess.textContent = 'Спасибо! Наши менеджеры свяжутся с вами в ближайшее время'
-        textSuccess.style.color = '#114B5F';
-        textSuccess.style.width = '400px';
-        textSuccess.style.lineHeight = '30px';
-        textSuccess.style.textAlign = 'left';
-        textSuccess.style.margin = '50px';
-    
-    
-        
-         form.addEventListener('submit' , (e) => {
-            e.preventDefault();
-            form.replaceWith(textSuccess)
-    
-        })
-        
-         btnClose.addEventListener('click', (e) => {
-            e.preventDefault();
-            wrapperConnection.classList.remove('openForm');
-        })
-    })
+        const btnShow = document.querySelector('#btn-show');
+        const wrapperConnection = document.querySelector('#wrapper-connection');
+        const btnClose = document.querySelector('#btn-close');
+        const form = document.querySelector('#form')
+        const message = document.querySelector('#message');
+        const input = document.querySelector('#input')
+        let isFormActive = true;
+   
+   
+        btnShow.addEventListener('click', (e) => {
+           e.preventDefault();
+           wrapperConnection.classList.toggle('openForm')
+       })
+   
+   
+       
+       form.addEventListener('submit' , (e) => {
+           e.preventDefault();
+           form.style.display = 'none';
+           message.style.display = 'block';
+           input.value = '';
+           isFormActive = false;
+       })
+       
+        btnClose.addEventListener('click', (e) => {
+           e.preventDefault();
+           wrapperConnection.classList.remove('openForm');
+           form.style.display = 'block';
+           message.style.display = 'none';
+           input.value = '';
+           isFormActive = false;
+       })
+   })
 }
 
 
