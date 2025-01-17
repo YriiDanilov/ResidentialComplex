77a54f7b79cd1812373f2af28b084854e7b43d72
yriidanilov <relantii@mail.ru>
2025-01-18 00:17:58 +0300

    fix

--- src/js/main_page_index.js
+++ src/js/main_page_index.js
@@ -1,12 +1,15 @@
 import '../style/main_page_style.css'
-import { preloader } from './main_page_preloader.js'
+
 import { fallMenu } from './main_page_fallMenu.js'
 import { splitWords } from './main_page_animationLetters.js'
 import { feedback } from './main_page_feedback.js'
-import { swiper } from './main_page_swiperSettings.js'
+import './main_page_swiperSettings.js'
+import './swiper-bundle.min.js'
+import { preloader } from './main_page_preloader.js'
 
-preloader()
+
 fallMenu()
 splitWords()
 feedback()
-swiper
+preloader()
+// swiper
