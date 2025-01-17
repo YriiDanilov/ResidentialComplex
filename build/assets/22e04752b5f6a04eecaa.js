8c9b3d94fbba4f4bdcaa7a0dcc620386a8048911
yriidanilov <relantii@mail.ru>
2025-01-17 23:02:47 +0300

    fix

---
+++ build/assets/22e04752b5f6a04eecaa.js
@@ -0,0 +1,1 @@
+export var preloader=function(){window.onload=function(){document.body.classList.add("loaded_hiding"),window.setTimeout((function(){document.body.classList.add("loaded"),document.body.classList.remove("loaded_hiding")}),500)}};
